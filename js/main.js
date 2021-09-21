1853f0a3c1635cea4f2222cdc6dc2ddc4c05eec0
fluke006 <johongirovdiyorbek08@gmail.com>
2021-09-21 18:04:31 +0500

    add

---
+++ js/main.js
@@ -0,0 +1,49 @@
+var elForm = document.querySelector('.form');
+
+var userVerb = document.querySelector('.user__verb');
+
+var userName = document.querySelector('.user__name');
+
+var elList = document.querySelector('.list');
+
+var todo = [];
+
+elForm.addEventListener('submit' , function (evt){
+
+   evt.preventDefault();
+
+   var userVerbValue = userVerb.value.trim();
+
+   userVerb.value = null;
+
+   var userNameValue = userName.value.trim();
+
+   userName.value = null;
+
+   elList.innerHTML = null
+
+   var todos = {
+      verb: userVerbValue,
+      name: userNameValue
+   };
+
+   todo.push(todos);
+
+   for (const itemTodos of todo) {
+      var listItem = document.createElement('li');
+      listItem.setAttribute('class', 'list__item');
+      listItem.textContent = itemTodos.verb + ' ';
+
+      var itemStrong  = document.createElement('strong');
+      itemStrong.setAttribute('class', 'item__strong');
+      itemStrong.textContent = itemTodos.name;
+
+      listItem.appendChild(itemStrong)
+
+      elList.appendChild(listItem);
+   }
+
+
+
+
+});
